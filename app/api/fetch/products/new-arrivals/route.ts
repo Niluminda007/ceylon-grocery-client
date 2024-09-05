ec26261ad1c2758ded92e104ba94f9b236f1e05e
Niluminda007 <princeofpersiatwathrones@gmail.com>
2024-09-05 15:08:42 +0300

    added forced dynamic into several api calls

--- app/api/fetch/products/new-arrivals/route.ts
+++ app/api/fetch/products/new-arrivals/route.ts
@@ -1,6 +1,5 @@
+export const dynamic = "force-dynamic";
 import { db } from "@/lib/db";
-
-import { Product } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
