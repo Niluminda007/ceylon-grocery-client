67d7461adbf17b38d9b7eb27b6d672c65ec8bd5e
Niluminda007 <princeofpersiatwathrones@gmail.com>
2024-12-13 11:54:08 +0200

    changed the slider images

--- constants/slider.ts
+++ constants/slider.ts
@@ -1,27 +1,12 @@
 export const slides = [
-  // {
-  //   id: 1,
-  //   image:
-  //     "https://res.cloudinary.com/dij3i3ar9/image/upload/v1720631514/eshop-ceylon/image_slider/slider_image_4_imlhcp.jpg",
-  // },
-  // {
-  //   id: 2,
-  //   image:
-  //     "https://res.cloudinary.com/dij3i3ar9/image/upload/v1708008424/eshop-ceylon/image_slider/slider_img_2_bs6qed.png",
-  // },
-  // {
-  //   id: 3,
-  //   image:
-  //     "https://res.cloudinary.com/dij3i3ar9/image/upload/v1708008424/eshop-ceylon/image_slider/slider_img_1_q8wl22.png",
-  // },
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dij3i3ar9/image/upload/v1731233261/ceylon-grocery/sliders/hero-slider/homedelivery_l5u1g8.png",
+      "https://res.cloudinary.com/dij3i3ar9/image/upload/v1734083580/ceylon-grocery/sliders/hero-slider/homedelivery_banner_2_rn380v.jpg",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dij3i3ar9/image/upload/v1731233261/ceylon-grocery/sliders/hero-slider/mainbanner_ob91is.png",
+      "https://res.cloudinary.com/dij3i3ar9/image/upload/v1734083580/ceylon-grocery/sliders/hero-slider/homedelivery_banner_1_hssifi.jpg",
   },
 ];
