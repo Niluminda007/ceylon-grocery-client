6ed548bdc95a9a2d8a001ee710c2f354a75e1c37
Niluminda007 <princeofpersiatwathrones@gmail.com>
2025-04-09 15:39:59 +0300

    re ordered delivery options

--- app/api/fetch/delivery-option/route.ts
+++ app/api/fetch/delivery-option/route.ts
@@ -11,13 +11,19 @@ export async function GET(req: NextRequest) {
       return NextResponse.json({ message: "Unauthorized!!!" }, { status: 401 });
     }
 
-    const deliveryOptions = await db.deliveryOption.findMany({
-      orderBy: {
-        createdAt: "asc",
-      },
-    });
+    const deliveryOptions = await db.deliveryOption.findMany();
+    const customOrder = [
+      "Standard",
+      "Express",
+      "Pick up myself",
+      "International Delivery",
+    ];
 
-    return NextResponse.json(deliveryOptions, { status: 200 });
+    const sortedDeliveryOptions = deliveryOptions.sort(
+      (a, b) => customOrder.indexOf(a.method) - customOrder.indexOf(b.method)
+    );
+
+    return NextResponse.json(sortedDeliveryOptions, { status: 200 });
   } catch (error) {
     console.error("Error fetching delivery options:", error);
     return NextResponse.json(
